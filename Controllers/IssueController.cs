759e7b74fc168370ab6273822c03f042c783ccf9
Sharat Chandra <sharatchandra897@gmail.com>
2023-02-25 18:32:49 +0530

    Rest of the Api's

--- Controllers/IssueController.cs
+++ Controllers/IssueController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;//
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +27,40 @@ namespace Tracking.Controllers
         public async Task<IEnumerable<Issue>> Get()
         {
             return await _context.Issues.ToListAsync();
+        }
+
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetbyId(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetbyId), new { id = issue.Id }, issue);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(int id, Issue issue)
+        {
+            if (id != issue.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
